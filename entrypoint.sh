2165829deae31dc06c6e6725bdc8f36351e650a1
QuentinN42 <quentin@lieumont.fr>
2023-03-05 12:31:21 -0800

    feat: env
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,18 +14,46 @@ set -e
 set -o pipefail
 
 
+# # Utils functions
+
+
 function raise(){
     error="${1:-"Unknown error"}"
     echo "Error: ${error}" >&2
     exit 1
 }
 
+
+function check_env() {
+    for var in "$@"; do
+        if [[ -z "${!var}" ]]; then
+            raise "${var} is not set"
+        fi
+    done
+}
+
+
+# # Vault functions
+
+
 function read_vault() {
     vault read -format=json "${1}" | jq -re ".data"
 }
+
 
 function login_vault() {
     echo "Logging in to vault..."
     vault login -no-print -non-interactive -method=userpass username="${VAULT_USER}" password="${VAULT_PASS}" || exit 1
 }
 
+
+# # Main
+
+
+function main() {
+    check_env VAULT_ADDR VAULT_PASS VAULT_USER
+
+    login_vault
+
+    bash
+}
