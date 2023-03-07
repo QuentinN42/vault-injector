6f69a68b1fedcad62d11c1db994c2e19f86e75d1
QuentinN42 <quentin@lieumont.fr>
2023-03-06 20:12:48 -0800

    feat: better logs
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 mod k8s;
 use k8s::get_pod_annotations;
+use log::{debug, error, info};
 
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
+    debug!("Starting annotation collection.\n");
+
     let annotations = match get_pod_annotations().await {
         Ok(data) => data,
         Err(e) => {
-            println!("Error: {}", e);
+            error!("Error: {}", e);
             std::process::exit(1);
         }
     };
+
+    if ! annotations.contains_key("vault-injector.io/config") {
+        info!("No vault-injector.io/config annotation found.\n");
+        debug!("Exiting.\n");
+        std::process::exit(0);
+    }
 
     dbg!(annotations);
 }
