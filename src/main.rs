7c7c0ea8ba414c485ae0d1bb3e7a29c7f83d125b
QuentinN42 <quentin@lieumont.fr>
2023-03-07 00:24:48 -0800

    feat: vault
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use log::{debug, error, info, trace};
+use std::{thread, time};
 
 mod k8s;
 use k8s::{get_annotations, set_env};
@@ -7,25 +8,37 @@ mod config;
 use config::parse_config;
 
 mod vault;
-use vault::resolve_env;
+use vault::Vault;
+
+use crate::config::{Config, Env};
+
+static SLEEP_TIME: time::Duration = time::Duration::from_secs(10);
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    let vault = match Vault::new().await {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Unable to log into vault : {}", e);
+            return;
+        }
+    };
 
     loop {
         trace!("Running loop.");
-        match run().await {
+        match run(&vault).await {
             Ok(_) => { },
             Err(e) => {
                 error!("Error: {}", e);
             }
         };
+        thread::sleep(SLEEP_TIME);
     }
 }
 
 
-async fn run() -> Result<(), Box<dyn std::error::Error>> {
+async fn run(vault: &Vault) -> Result<(), Box<dyn std::error::Error>> {
     debug!("Starting annotation collection.");
 
     let annotations = get_annotations().await?;
@@ -40,7 +53,7 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     debug!("Config: {:?}", config);
 
-    let resolved = resolve_env(&config)?;
+    let resolved = vault.resolve_env(&config).await?;
 
     info!("Resolved {} variables to inject.", resolved.len());
 
--- src/vault/mod.rs
+++ src/vault/mod.rs
@@ -1,8 +1,14 @@
 use std::error::Error;
 
+use vaultrs::{client::{VaultClient, VaultClientSettingsBuilder}, auth::userpass, api::{kv2::requests::{ReadSecretRequest}, self}, sys::wrapping::lookup, token::lookup_self};
 use log::{debug, trace, warn};
 
 use crate::config::Config;
+
+pub struct Vault {
+    client: VaultClient,
+}
+
 
 #[derive(Debug)]
 pub struct Env {
@@ -10,26 +16,64 @@ pub struct Env {
     pub value: String,
 }
 
-pub fn resolve_env(_config: &Config) -> Result<Vec<Env>, Box<dyn Error>> {
-    trace!("Resolving env variables.");
-    let mut env: Vec<Env> = vec![];
-    let mut seen_names: Vec<String> = vec![];
+impl Vault {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let endpoint = std::env::var("VAULT_ADDR")?;
+        let user = std::env::var("VAULT_USER")?;
+        let pass = std::env::var("VAULT_PASS")?;
 
-    for e in &_config.env {
-        debug!("Resolving env variable: {}", e.name);
-        env.push(Env {
-            name: e.name.clone(),
-            value: get_secret(&e.engine, &e.secret, &e.field).unwrap(),
-        });
-        if seen_names.contains(&e.name) {
-            warn!("Duplicate env variable name: {}", e.name);
-        }
-        seen_names.push(e.name.clone());
+        // login to retrieve token
+        let settings = VaultClientSettingsBuilder::default()
+            .address(endpoint.clone())
+            .build()
+            .unwrap();
+        let tmp = VaultClient::new(settings)?;
+        let auth = userpass::login(
+            &tmp,
+            "userpass",
+            &user,
+            &pass,
+        ).await.unwrap();
+
+        // then set the token as current context
+        let settings = VaultClientSettingsBuilder::default()
+            .address(endpoint)
+            .token(auth.client_token)
+            .build()
+            .unwrap();
+        let client = VaultClient::new(settings)?;
+
+        Ok(Vault { client })
     }
 
-    Ok(env)
-}
 
-fn get_secret(_engine: &str, _secret: &str, _field: &str) -> Result<String, Box<dyn Error>> {
-    Ok(String::from("test"))
+    pub async fn resolve_env(&self, _config: &Config) -> Result<Vec<Env>, Box<dyn Error>> {
+        trace!("Resolving env variables.");
+        let mut env: Vec<Env> = vec![];
+        let mut seen_names: Vec<String> = vec![];
+
+        for e in &_config.env {
+            debug!("Resolving env variable: {}", e.name);
+            env.push(Env {
+                name: e.name.clone(),
+                value: self.get_secret(&e.engine, &e.secret, &e.field).await.unwrap(),
+            });
+            if seen_names.contains(&e.name) {
+                warn!("Duplicate env variable name: {}", e.name);
+            }
+            seen_names.push(e.name.clone());
+        }
+
+        Ok(env)
+    }
+
+    pub async fn get_secret(&self, engine: &str, secret: &str, field: &str) -> Result<String, Box<dyn Error>> {
+        let endpoint = ReadSecretRequest::builder()
+            .mount(engine)
+            .path(secret)
+            .build()
+            .unwrap();
+        let res = api::exec_with_result(&self.client, endpoint).await?;
+        Ok(res.data.get(field).unwrap().to_string())
+    }
 }
