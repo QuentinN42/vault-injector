ae67050f8a6de4beb1b94493577715466a148144
QuentinN42 <quentin@lieumont.fr>
2023-03-06 19:12:13 -0800

    feat: get pod
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use kube::{api::Api, Client};
+use k8s_openapi::api::core::v1::Pod;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::namespaced(client, "default");
+
+    let pod = pods.get("podname").await?;
+    dbg!(pod);
+
+    Ok(())
 }
